b82ad43a840e160d850e47eedb8e436cf31632c1
Gustav Näslund <gustav@gntech.se>
2018-06-04 19:06:56 +0200

    various, add config file reader via viper, add open function

--- go.mod
+++ go.mod
@@ -1,6 +1,19 @@
 module github.com/gntech/prjbrain
 
 require (
+	github.com/fsnotify/fsnotify v1.4.7
 	github.com/gorilla/mux v1.6.2
+	github.com/hashicorp/hcl v0.0.0-20180404174102-ef8a98b0bbce
+	github.com/magiconair/properties v1.8.0
+	github.com/mitchellh/mapstructure v0.0.0-20180511142126-bb74f1db0675
+	github.com/pelletier/go-toml v1.1.0
+	github.com/spf13/afero v1.1.0
+	github.com/spf13/cast v1.2.0
+	github.com/spf13/jwalterweatherman v0.0.0-20180109140146-7c0cea34c8ec
+	github.com/spf13/pflag v1.0.1
+	github.com/spf13/viper v1.0.2
 	github.com/tealeg/xlsx v1.0.3
+	golang.org/x/sys v0.0.0-20180525142821-c11f84a56e43
+	golang.org/x/text v0.3.0
+	gopkg.in/yaml.v2 v2.2.1
 )
--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 	"github.com/tealeg/xlsx"
 )
 
@@ -21,7 +25,86 @@ type Doc struct {
 	Path     string
 	Revision string
 	Markdown bool
-	Files    []string
+	Files    []File
+}
+
+// File struct
+type File struct {
+	FilePath string
+	RelPath  string
+}
+
+// Global variables
+var docMap map[string]*Doc
+var projectNumber string
+var projectName string
+
+func main() {
+	if len(os.Args) > 1 {
+		viper.SetConfigFile(os.Args[1]) // Prefer to use config file provided as argument
+	} else {
+		viper.SetConfigName("config") // name of config file (without extension)
+		viper.AddConfigPath(".")      // optionally look for config in the working directory
+	}
+
+	// Add default values to options if not set in config file
+	viper.SetDefault("pn_start_row", 4)
+	viper.SetDefault("title_col", 1)
+	viper.SetDefault("docnr_col", 2)
+	viper.SetDefault("number_log", "testfiles/Nummerliggare.xlsm")
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Fatalf("fatal error config file: %s", err)
+	}
+
+	rootDir := getInputDir(viper.ConfigFileUsed())
+	absRootDir, err := filepath.Abs(rootDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initDocMap(viper.GetString("number_log"))
+	searchForDocs(rootDir)
+
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.HandleFunc("/", overviewHandler)
+	r.HandleFunc("/details", detailsHandler)
+	r.HandleFunc("/other", otherHandler)
+	r.HandleFunc("/files", filesHandler)
+	http.Handle("/", r)
+
+	addr := "localhost:8000"
+	srv := &http.Server{
+		Handler: r,
+		Addr:    addr,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	fmt.Println("Prjbrain is monitoring " + absRootDir + " and serving at " + addr)
+	go open("http://" + addr)
+	log.Fatal(srv.ListenAndServe())
+}
+
+// open opens the specified URL or file from the file system in the systems default application.
+// From github.com/icza/gowut
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
 }
 
 func overviewHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,6 +125,24 @@ func otherHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, p)
 }
 
+// Files handler opens a local file and then returns to the overview page.
+func filesHandler(w http.ResponseWriter, r *http.Request) {
+	go open(r.URL.Query().Get("path"))
+	http.Redirect(w, r, "/", 301)
+}
+
+func getInputDir(cf string) string {
+	p := path.Dir(cf)
+	if path.IsAbs(p) {
+		return p
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Cant find current working directory %v", err)
+	}
+	return path.Join(wd, p)
+}
+
 func searchForDocs(rootDir string) {
 	subDirToSkip := ".git" // dir/to/walk/skip
 
@@ -59,7 +160,11 @@ func searchForDocs(rootDir string) {
 			if strings.HasPrefix(info.Name(), projectNumber) {
 				for k, v := range docMap {
 					if strings.HasPrefix(info.Name(), k) {
-						docMap[k].Files = append(v.Files, path)
+						relPath, err := filepath.Rel(rootDir, path)
+						if err != nil {
+							log.Fatalf("Cant find current working directory %v", err)
+						}
+						docMap[k].Files = append(v.Files, File{FilePath: path, RelPath: relPath})
 						// fmt.Println(v.Files)
 					}
 				}
@@ -94,52 +199,18 @@ func initDocMap(nrLogFile string) {
 	// Initialize the map that will hold all the docs in the project
 	docMap = make(map[string]*Doc)
 
-	for _, row := range sheet.Rows[4:] {
-		docnr, err := row.Cells[2].FormattedValue()
+	for _, row := range sheet.Rows[viper.GetInt("pn_start_row"):] {
+		docnr, err := row.Cells[viper.GetInt("docnr_col")].FormattedValue()
 		if err != nil {
 			log.Fatal(err)
 		}
 		if docnr != "" {
 			// nr := row.Cells[0].String()
-			title := row.Cells[1].String()
+			title := row.Cells[viper.GetInt("title_col")].String()
 
 			// Initialize the project document map with the numbers from the number log.
 			docMap[docnr] = &Doc{Title: title, DocNr: docnr}
 			// fmt.Printf("%s : %s : %s\n", nr, docnr, title)
 		}
 	}
-}
-
-// Global variables
-var docMap map[string]*Doc
-var projectNumber string
-var projectName string
-
-func main() {
-	rootDir := "."
-	absRootDir, err := filepath.Abs(rootDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	initDocMap("testfiles/Nummerliggare.xlsm")
-	searchForDocs(rootDir)
-
-	r := mux.NewRouter()
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	r.HandleFunc("/", overviewHandler)
-	r.HandleFunc("/details", detailsHandler)
-	r.HandleFunc("/other", otherHandler)
-	http.Handle("/", r)
-
-	addr := "127.0.0.1:8000"
-	srv := &http.Server{
-		Handler: r,
-		Addr:    addr,
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-	fmt.Println("Prjbrain is monitoring " + absRootDir)
-	fmt.Println("Please go to " + addr + " in your favourite web browser")
-	log.Fatal(srv.ListenAndServe())
 }
